9ec2ed0e53b451d7522177d8b61ca14c6ac6fd8b
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-06 20:02:19 +0200

    Update loginexe.php

--- loginexe.php
+++ loginexe.php
@@ -1,5 +1,8 @@
 <?php
 
+
+
+    /*
     //SIDIRO 2
     $host='localhost';
     $db = 'sindopoly_db';
@@ -20,29 +23,32 @@
         $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
     //END SIDIRO 2
+    */
 
-    /*
     //KAREZOS CONNECT
     $host='localhost';
     $db = 'sindopoly_db';
 
 
     $user='it175008';
-    $pass='#Mementomori199';
+    $pass='';
 
 
     if(gethostname()=='users.iee.ihu.gr') {
         $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it175008/mysql/run/mysql.sock');
+        
+        
     } else {
             $mysqli = new mysqli($host, $user, $pass, $db);
     }
 
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: (" . 
-    $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-    //END OF KAREZOS CONNECT
-    */
+        $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+    else {
+        echo ("connected");
+    }
 
     /*
     //SIDIRO CONNECT
