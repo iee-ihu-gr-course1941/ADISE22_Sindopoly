3bdc23a04dc919688d8ad769d47a4d5d9ca6bdb2
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-06 21:40:45 +0200

    Update registerexe.php

--- registerexe.php
+++ registerexe.php
@@ -2,13 +2,13 @@
 
     //CONNECTION BLOCK--------------------------------------------------------------------------
     //mysqli_connect("127.0.0.1","it175008","PASSWORD","sindopoly",3333);//users
-    $conn = new mysqli("127.0.0.1","it175008","#Mementomori199","sindopoly_db",3333);//users
+    $mysqli = new mysqli("127.0.0.1","it175008","#Mementomori199","sindopoly_db",3333);//users
     //$mysqli=new mysqli("127.0.0.1","it175008","PASSWORD","sindopoly",3333);
-    if (!$conn) {
+    if (!$mysqli) {
         die("Connection failed: " . mysqli_connect_error());
     }else{
         echo "Connection successful";
-        $result=mysqli_query($conn,"SELECT DATABASE()");//print default database
+        $result=mysqli_query($mysqli,"SELECT DATABASE()");//print default database
         $row = mysqli_fetch_row($result);
         printf("<br>Default database is %s", $row[0],"<br>");
     }
@@ -18,7 +18,7 @@
     
     //FIND LAST ID
     $sql = "SELECT * FROM player ORDER BY id DESC LIMIT 1";
-    $sqlres=mysqli_query($conn,$sql);
+    $sqlres=mysqli_query($mysqli,$sql);
     $sqlarr=mysqli_fetch_array($sqlres);
     $result=$sqlarr["id"];
     
@@ -29,7 +29,7 @@
     //CHECK IF USERNAME ALREADY EXISTS
     $found=0;//STAYS 0 IF IDENTICAL NAME ISNT FOUND
     $sql="SELECT name FROM player";
-    $result2=mysqli_query($conn,$sql);
+    $result2=mysqli_query($mysqli,$sql);
     while($row = mysqli_fetch_array($result2)) {
         if($name==$row["name"]){//COMPARE ALL NAME ROWS FROM DB WITH NAME GIVEN
             $found=1;
@@ -48,10 +48,10 @@
     //IF NO IDENTICAL USER FOUND THEN CREATE NEW USER
     if($found==0){
         $sql="INSERT INTO player(id,name,pass) VALUES ('$id','$name','$pass')";
-        if ($conn->query($sql) === TRUE) {
+        if ($mysqli->query($sql) === TRUE) {
         echo "New record created successfully";
       } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: " . $sql . "<br>" . $mysqli->error;
       }
     }elseif ($found==1){
         echo "<br>User already registered in database, no new user created";
