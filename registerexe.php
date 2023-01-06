03de9a244b17a6eb5000a62a387a1c2f6b07ac5f
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-06 19:45:58 +0200

    a

--- registerexe.php
+++ registerexe.php
@@ -10,7 +10,7 @@
 
 
     if(gethostname()=='users.iee.ihu.gr') {
-        $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/staff/asidirop/mysql/run/mysql.sock');
+        $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it175008/mysql/run/mysql.sock');
     } else {
             $mysqli = new mysqli($host, $user, $pass, $db);
     }
