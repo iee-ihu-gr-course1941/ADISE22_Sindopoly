4e4556cea1528bce09f1b3bd7c22d4eef2d95d10
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-07 18:09:16 +0200

    Update S_Join.php

--- S_Join.php
+++ S_Join.php
@@ -24,10 +24,10 @@
     }
 
     if($found==1){
-        echo "Welcome Player 1".$joinname;
+        echo "<br>Welcome Player 1 : ".$joinname."<br>";
         $youare=1;
     }else if($found==2){
-        echo "Welcome Player 2".$joinname;
+        echo "<br>Welcome Player 2 :".$joinname."<br>";
         $youare=2;
     }else{
         echo "User not found in game nickname given.Try again";
