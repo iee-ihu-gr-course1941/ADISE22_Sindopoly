cf6259aba8e25c9c98d77a162faf1dcd52c4c373
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-07 13:28:51 +0200

    Update SindopolyLogin.php

--- SindopolyLogin.php
+++ SindopolyLogin.php
@@ -13,29 +13,20 @@
         } else {
             $mysqli = new mysqli($host, $user, $pass, $db);
         }
-        /*
-        if ($mysqli->connect_errno) {
-            echo "Failed to connect to MySQL: (" . 
-            $mysqli->connect_errno . ") " . $mysqli->connect_error;
-        }else{
-            echo "Connection successful";
-            echo "<script>document.getElementById('element').innerHTML = 'Connection with Sindopoly successful';</script>";
-        }
-        */
+        
         //MASTER CONNECT
         ?>
     </head>
         <body>
             <div class="mid">
                 <h1>Welcome to Sindopoly</h1>
-                <p id="constatus">
+                <p>---Connection Status---</p>
+                <p>
                     <?php
                         if ($mysqli->connect_errno) {
-                            echo "Failed to connect to MySQL: (" . 
-                            $mysqli->connect_errno . ") " . $mysqli->connect_error;
+                            echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
                         }else{
                             echo "Connection successful";
-                            /*echo "<script>document.getElementById('element').innerHTML = 'Connection with Sindopoly successful';</script>";*/
                         }
                     ?>
                 </p>
