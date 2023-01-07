0a1bed641014974aeab8bbf6d46a59c01ceaceeb
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-07 22:15:50 +0200

    Update karezos.php

--- karezos.php
+++ karezos.php
@@ -2,22 +2,22 @@
 <html>
   <head>
     <title>My PHP Page</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
   </head>
   <body>
     <h1>My PHP Page</h1>
-    <button id="btn-send-request">Send Request</button>
-    <div id="response-container">fk</div>
+    <button id="btn">Send Request</button>
+    <div id="rescon">fk</div>
     
     <script>
       $(document).ready(function () {
-        $('#btn-send-request').click(function () {
+        $('#btn').click(function () {
           $.ajax({
             type: 'POST',
             url: 'login.php',
             data: { action: 'greet', name: 'John' },
             success: function (response) {
-              $('#response-container').html(response);
+              $('#rescon').html(response);
             },
             error: function (error) {
               console.error('Error:', error);
