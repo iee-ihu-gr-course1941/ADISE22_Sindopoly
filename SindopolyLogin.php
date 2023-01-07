5083539162f70c06c4fc0521d24a88b7fef4f594
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-07 13:15:31 +0200

    Update SindopolyLogin.php

--- SindopolyLogin.php
+++ SindopolyLogin.php
@@ -1,90 +1,19 @@
-<?php
-
-    //MASTER CONNECT
-    $host='localhost';
-    $db = 'sindopoly_db';
-    $user='it175008';
-    $pass='';
-    if(gethostname()=='users.iee.ihu.gr') {
-        $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it175008/mysql/run/mysql.sock');
-    } else {
-        $mysqli = new mysqli($host, $user, $pass, $db);
-    }
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: (" . 
-        $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    }else{
-        echo ("Connected to Database of Sindopoly<br>");
-    }
-    //MASTER CONNECT
-
-    /*
-    //CONNECTION BLOCK--------------------------------------------------------------------------
-    //mysqli_connect("127.0.0.1","it175008","PASSWORD","sindopoly",3333);//users
-    $mysqli = new mysqli("127.0.0.1","it175008","#Mementomori199","sindopoly_db",3333);//users
-    //$mysqli=new mysqli("127.0.0.1","it175008","PASSWORD","sindopoly",3333);
-    if (!$mysqli) {
-        die("Connection failed: " . mysqli_connect_error());
-    }else{
-        echo "Connection successful";
-        $result=mysqli_query($mysqli,"SELECT DATABASE()");//print default database
-        $row = mysqli_fetch_row($result);
-        printf("<br>Default database is %s", $row[0],"<br>");
-    }
-    //CONNECTION BLOCK--------------------------------------------------------------------------
-   */
-    
-    
-    //FIND LAST ID
-    $sql = "SELECT * FROM player ORDER BY id DESC LIMIT 1";
-    $sqlres=mysqli_query($mysqli,$sql);
-    $sqlarr=mysqli_fetch_array($sqlres);
-    $result=$sqlarr["id"];
-    
-    $id=$result+1;
-    $name=$_GET['name'];
-    $pass=$_GET['pass'];
-    
-    //CHECK IF USERNAME ALREADY EXISTS
-    $found=0;//STAYS 0 IF IDENTICAL NAME ISNT FOUND
-    $sql="SELECT name FROM shitposts";
-    $result2=mysqli_query($mysqli,$sql);
-    while($row = mysqli_fetch_array($result2)) {
-        if($name==$row["name"]){//COMPARE ALL NAME ROWS FROM DB WITH NAME GIVEN
-            $found=1;
-            //echo "<br>Comparing: ".$name." with: ".$row["name"];
-        }
-    }
-    
-
-    
-    if($found==0){
-        echo "<br>User doesnt exist<br>";
-    }else{
-        echo "<br>User exists<br>";
-    }
-    
-    //IF NO IDENTICAL USER FOUND THEN CREATE NEW USER
-    if($found==0){
-        $sql="INSERT INTO shitposts(id,name,pass) VALUES ('$id','$name','$pass')";
-        if ($mysqli->query($sql) === TRUE) {
-        echo "<br>New record created successfully<br>";
-      } else {
-        echo "Error: " . $sql . "<br>" . $mysqli->error;
-      }
-    }elseif ($found==1){
-        echo "<br>User already registered in database, no new user created<br>";
-    }
-
-    //FUCK ABOUT
-    echo "<p><h1><b>Shitpost Leaderboard</b></h1></p>";
-    $sql = "SELECT * FROM shitposts";
-       
-        $result=mysqli_query($mysqli,$sql);
-        while($row = mysqli_fetch_array($result)) {
-            echo "<br>".$row["id"]." ".$row["name"]. "_" . $row["pass"].".";
-        }
-
-
-
-?>
+<html>
+    <head>
+        <title>Sindopoly Login</title>
+        <link rel="stylesheet" type="text/css" href="SindopolyLogin.css">
+        
+    </head>
+        <body>
+            <div class="mid"><h1>Welcome to Sindopoly</h1></div>
+            <div class="half">
+                <h1>Create new game</h1>
+              </div>
+              <div class="half">
+                <h1>Login in existing game</h1>
+              </div>
+              <?php echo "help";?>
+              
+            
+        </body>
+</html>
