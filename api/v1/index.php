b4e1573b344ae8b746a239cdce2a35eeb643162c
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-08 20:26:00 +0200

    Update index.php

--- api/v1/index.php
+++ api/v1/index.php
@@ -18,7 +18,15 @@ $input = json_decode(file_get_contents('php://input'),true);
 
 //$GLOBALS['input'] = json_decode(file_get_contents('php://input'), true);
 
+global $myqsli;
+    $sql="SELECT gamename,p1name,p2name FROM sgame";
+    $st=$mysqli->prepare($sql);
 
+    $st->execute();
+    $res = $st->get_result();
+
+    header('Content-type: application/json');
+    print json_encode($res->fetch_all(MYSQLI_ASSOC),JSON_PRETTY_PRINT);
 
 switch ($request[0]) {
     case 'exchange':
