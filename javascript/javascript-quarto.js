0d4c2279a93d0be1f765dd59a9274ae521c3bbe8
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-08 16:28:11 +0200

    Update javascript-quarto.js

--- javascript/javascript-quarto.js
+++ javascript/javascript-quarto.js
@@ -5,10 +5,11 @@
 var x = { username: null, pass: null };
 
 $(function() {
-    $('#inbut').click(login_to_game);
+    $('#inbut').click(givedata);
+    $('#outbut').click(getdata);
 });
 
-function login_to_game() {
+function givedata() {
     $.ajax({
         url: "api/v1/index.php/login",
         method: 'POST',
@@ -33,18 +34,7 @@ function getdata() {
 
 }
 
-function regist_to_game() {
-    $.ajax({
-        url: "api/v1/register/",
-        method: 'GET',
-        dataType: "json",
-        contentType: 'application/json',
-        data: JSON.stringify({ username: $('#username').val(), pass: $('#pass').val() }),
-        // success: success,
-        error: login_error
-    });
 
-}
 
 function onsuccess(data) {
 
@@ -55,25 +45,3 @@ function onsuccess(data) {
     alert("good job");
 }
 
-function login_error(data) {
-
-    var errorm = data.errormesg;
-    alert(errorm);
-}
-
-function update_info() {
-    $('#player_info').html("<h4>Player info</h4> " +
-        "<strong> Username:</strong>" + me.username +
-        "<strong> id: </strong>" + me.player_id +
-        "<strong> token: </strong>" + me.$_COOKIE['tokenC'] +
-        "<strong> Player role: </strong> " + me.role +
-        "<br><h4>Game info</h4>" +
-        "<strong> Game state: </strong>" + game_status.status +
-        "<strong> Player turn: </strong>" + game_status.turn +
-        "<strong> Current Piece: </strong>" + game_status.current_piece +
-        "<strong> Result: </strong>" + game_status.result);
-}
-
-function save_player_info(data) {
-    me = data[0];
-}
