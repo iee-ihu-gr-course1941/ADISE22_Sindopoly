9ef064fb279c3d751a2f1d68826cc9ec58c3641b
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-08 16:19:50 +0200

    Update index.php

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,12 +1,16 @@
 <?php
 define('Access', TRUE);
-
+echo "1<br>";
 require_once "lib/dbconnect.php";
+echo "2<br>";
 require_once "lib/login.php";
+echo "3<br>";
 require_once "lib/game.php";
+echo "4<br>";
 require_once "lib/board.php";
+echo "1<br>";
 
-echo "wtf<br>";
+
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 $GLOBALS['input'] = json_decode(file_get_contents('php://input'), true);
@@ -21,7 +25,7 @@ echo "<br>FM FR";
 switch ($request[0]) {
     case 'login':
         if ($method == 'POST') {
-            login();
+            //login();
         } else {
             header("HTTP/1.1 400 Bad Request");
             print json_encode(['errormesg' => "Method $method not allowed here."]);
