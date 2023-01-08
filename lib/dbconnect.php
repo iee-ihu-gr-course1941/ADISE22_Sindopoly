55343eae8d5ace37771b2179f269051b7e9f2ffa
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-08 10:23:17 +0200

    Update dbconnect.php

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -8,7 +8,7 @@
 
     //NEW MASTER CONNECT
     if(gethostname()=='users.iee.ihu.gr') {
-        $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/staff/asidirop/mysql/run/mysql.sock');
+        $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it175008/mysql/run/mysql.sock');
         } else {
             $pass=null;
             $mysqli = new mysqli($host, $user, $pass, $db);
