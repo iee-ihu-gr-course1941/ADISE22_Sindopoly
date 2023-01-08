ada590defe3fc593b943e4455520c80eab2c2180
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-08 11:26:01 +0200

    Update dbconnect.php

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -12,6 +12,7 @@
         } else {
             $pass=null;
             $mysqli = new mysqli($host, $user, $pass, $db);
+            echo "CONNECTION DONE"
         }
         if ($mysqli->connect_errno) {
             echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
