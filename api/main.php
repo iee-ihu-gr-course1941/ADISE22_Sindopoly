3be2f8465af51992d2f6c2f1f0d17d29afb3db1d
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-09 13:43:38 +0200

    Update main.php

--- api/main.php
+++ api/main.php
@@ -10,16 +10,6 @@ $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 $input = json_decode(file_get_contents('php://input'),true);
 
-$gamename;
-$p1name;
-$p2name;
-//$request_path = trim($_SERVER['PATH_INFO'],'/');
-//$GLOBALS['input'] = json_decode(file_get_contents('php://input'), true);
-
-
-echo $gamename."<br>";
-echo $p1name."<br>";
-echo $p2name."<br>";
 echo json_encode($input);
 
 
