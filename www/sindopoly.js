c8b0cac2685a6ec6d74b2e00197211cae24bf93f
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-09 11:08:27 +0200

    qwe

--- www/sindopoly.js
+++ www/sindopoly.js
@@ -6,45 +6,17 @@ $(function() {
 });
 
 function givedata() {
-
     $.ajax({
         url: "../api/main.php/show",
         success: cry
     }
     );
-
-    /*
-    var p1=$("#p1name").val();
-    var p2=$("#p2name").val();
-    alert(p1,p2);
-
-    $.post("api/v1/index.php",
-    {
-        p1name: p1
-    },
-    function(res,status){
-        var data=JSON.parse(res);
-
-    })
-    
-
-
-
-
-
-
-    $.ajax({
-        url: "api/v1/index.php/exchange",
-        method: 'POST',
-        dataType: "json",
-        contentType: 'application/json',
-        data: JSON.stringify({ p1name: p1, p2name: p2 }),
-    });
-    */
 }
 
 function cry(data){
-    alert(data[0]);
+    var de=data[0];
+    alert(de);
+    $(resultdiv).html(de);
 }
 
 
