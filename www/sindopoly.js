e2f659d85256881c5a8bb9005f734d0910d35cef
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-09 13:23:18 +0200

    Update sindopoly.js

--- www/sindopoly.js
+++ www/sindopoly.js
@@ -1,18 +1,30 @@
 $(function() {
-    givedata();
-    $('#inbut').click(givedata);
+    //givedata();
+    $('#inbut').click(dome);
 
     //$('#outbut').click(getdata);
 });
 
-function givedata() {
+function dome() {
     $.ajax({
-        url: "../api/main.php/show",
-        success: cry
+        url: "../api/main.php/creategame",
+        //success: cry
+        method: "POST",
+        data:{
+        "gamename":"solid",
+        "p1name":"snake",
+        "p2name":"rules"
+        },
+        contentType: "application/json",
+        success: letsgo
     }
     );
 }
 
+
+function letsgo(){
+    alert("NICE JOB");
+}
 function cry(data){
     var gamename=data[0].gamename;
     var p1name=data[0].p1name;
