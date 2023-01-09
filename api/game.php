ca6806173c369c8f502dae8041b4f07420d550d5
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-09 20:48:09 +0200

    Update game.php

--- api/game.php
+++ api/game.php
@@ -264,7 +264,8 @@
 
     function printdb(){//PRINTS THE WHOLE DATABASE
         global $mysqli;//WORKS 100% !!PUT GLOBAL WHEN NEEDED
-        $sql="SELECT * FROM game";
+        global $gamename;
+        $sql="SELECT * FROM game WHERE gamename='$gamename'";
         $st=$mysqli->prepare($sql);
     
         $st->execute();
